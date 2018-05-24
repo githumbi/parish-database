fc66a77502a680ff454dd546435e66fcdd54a3d6
githumbi <githumbi74@gmail.com>
2018-05-25 01:45:48 +0300

    First commit- created front end for signup page

---
+++ login/db.php
@@ -0,0 +1,5 @@
+<?php
+
+//connect to database
+$conn = mysqli_connect('localhost','root','','parish');
+
---
+++ login/sql.sql
@@ -0,0 +1,5 @@
+CREATE TABLE login(
+	id int(128) AUTO_INCREMENT PRIMARY KEY not null,
+    name varchar(128) not null,
+    paswd varchar(128) not null
+);
