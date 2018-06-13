e45a9193ae16be1f6dbcfbf128e6cee338a1e39f
githumbi <githumbi74@gmail.com>
2018-06-13 18:35:14 +0300

    moddified edit and details button

--- admin/tithe.php
+++ admin/tithe.php
@@ -6,9 +6,9 @@
                 class="collapse navbar-collapse" id="navcol-1" style="background-color:#3b99e0;width:328px;margin:0;">
                 <ul class="nav navbar-nav ml-auto">
                     <li class="nav-item" role="presentation"><a class="nav-link" href="index.php">Home</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link active d-block" href="signup.php">SIGNUP MEMBERS</a></li>
-                    <li class="nav-item" role="presentation" style="padding:3px;"><a class="nav-link active" href="tithe.php">individual details</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="edit.php">Edit</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="signup.php">SIGNUP MEMBERS</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="tithe.php">individual details</a></li>
+                    
                     <li class="nav-item" role="presentation"><a class="nav-link" href="offering.php">weekly offering</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="registration.php">Register</a></li>
                 </ul><button class="btn btn-primary" type="button" style="background-color:rgb(13,230,34);">LOGOUT</button></div>
@@ -20,9 +20,21 @@
             <div class="row">
                 <div class="col-md-12 col-xl-4 offset-xl-4">
                     <h2 style="color:rgb(255,255,255);"><strong><span style="text-decoration: underline;">TITHE / OFFERING</span></strong></h2>
+                    <div class="row">
+                    </div>
                 </div>
             </div>
         </div>
+         <div class="container">
+                      <?php if (isset($_SESSION['msg'])):  ?>
+                                <div class="alert alert-success" role="alert"><span><strong>
+                                  <?php
+                                    echo $_SESSION['msg'];
+                                    unset($_SESSION['msg']);
+                                  ?>
+                                </strong></span><button class="close"><span aria-hidden="true">×</span></button></div>
+                                <?php endif ?>
+                        </div>
         <div class="container" style="background-color:#ffffff;"><table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
        <thead>
     <tr>
@@ -59,10 +71,10 @@
           <td><?php  echo $row['total4']; ?></td>
           <td><?php  echo $row['total5']; ?></td>
           <td>
-            <a href="../individual/edit.php?edit=<?php echo $row['id']; ?>" onclick="return confirm('Edit Details of selected Member?')">Edit</a>
+            <a href="edit.php?edit=<?php echo $row['id']; ?>" onclick="return confirm('Edit Details of selected Member?')">Edit</a>
           </td>
           <td>
-            <a href="../individual/tithe.php?edit=<?php echo $row['id']; ?>">Details</a>
+            <a href="individual.php?edit=<?php echo $row['id']; ?>">Details</a>
           </td> 
          
     
